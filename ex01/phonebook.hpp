1bacbd204d622007e3a11d166ab55d75bccecc10
Ryo Kawaharada <rkawahar@c2r5s8.42tokyo.jp>
2024-11-13 08:35:05 +0900

    fix

--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kawaharadaryou <kawaharadaryou@student.    +#+  +:+       +#+        */
+/*   By: rkawahar <rkawahar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 04:32:08 by kawaharadar       #+#    #+#             */
-/*   Updated: 2024/11/12 20:54:43 by kawaharadar      ###   ########.fr       */
+/*   Updated: 2024/11/13 08:34:33 by rkawahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <limits>
 # include <iostream>
 # include <sstream>
+# include <cstdlib>
 
 class PhoneBook {
 	private:
